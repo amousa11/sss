7f19e749d124455bcbefd218a6eb46ed4d518e8f
Ali Mousa <amousa11@gmail.com>
2018-04-08 21:59:45 -0700

    writing my first test in go

---
+++ sss_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+import "math/big"
+
+func TestSecretGeneration(t *testing.T) {
+
+	min := 100
+	shares := 1200
+	prime, _ := big.NewInt(1).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
+
+	secret, xs, ys := makeRandomShares(min, shares, prime)
+	recoveredSecret := recoverSecret(xs[:min], ys[:min], prime)
+
+	t.Log("Expect ", secret, "to equal", recoveredSecret)
+
+	if secret.Cmp(recoveredSecret) != 0 {
+		t.Errorf("Expected secret %x to equal recovered secret %x", secret, recoveredSecret)
+	}
+
+}
