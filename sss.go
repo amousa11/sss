8b39545e3444c91c45435c27d984c967a53082da
Ali Mousa <amousa11@gmail.com>
2018-06-03 15:17:22 -0700

    add error check for min shares, remove main fn

--- sss.go
+++ sss.go
@@ -2,55 +2,10 @@ package sss
 
 import (
 	"errors"
-	"fmt"
 	"math/big"
-	"os"
-	"sss/utils"
-	"strconv"
+
+	"github.com/amousa11/sss/utils"
 )
-
-func main() {
-
-	fmt.Println(os.Args)
-	args := os.Args[1:]
-
-	if len(args) < 2 {
-		panic("expected more arguments: ./sss minimum shares")
-	}
-
-	n, err := strconv.Atoi(args[0])
-	if err != nil {
-		panic(err.Error())
-	}
-
-	m, err := strconv.Atoi(args[1])
-	if err != nil {
-		panic(err.Error())
-	}
-
-	prime, _ := big.NewInt(1).SetString("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F", 16)
-
-	fmt.Println("FIELD ORDER =", prime.Text(16))
-	fmt.Println("Generating ", n, "shares with threshold", m, " for recovery:")
-	secret, points, e := GenerateShares(n, m, prime)
-	if e != nil {
-		panic(e.Error())
-	}
-	fmt.Println("Secret : ", secret.Text(16))
-
-	fmt.Println("Shares : ")
-	for i := 0; i < len(points); i++ {
-		fmt.Println(points[i].X.Text(16), "\t", points[i].Y.Text(16))
-	}
-
-	recoveredSecret, e := RecoverSecret(points[:n], prime)
-
-	if e != nil {
-		panic(e.Error())
-	}
-
-	fmt.Println("secret recovered from minimum subset of shares", recoveredSecret.Text(16))
-}
 
 // GenerateShares generates a number of shares which can only be recovered by the minimum number of shares
 func GenerateShares(minimum int, shares int, prime *big.Int) (*big.Int, []*utils.Point, error) {
@@ -58,6 +13,10 @@ func GenerateShares(minimum int, shares int, prime *big.Int) (*big.Int, []*utils
 	points := make([]*utils.Point, shares)
 
 	if minimum > shares {
+		return nil, nil, errors.New("Minimum number of shares specified is greater than the total number of shares")
+	}
+
+	if minimum < 2 {
 		return nil, nil, errors.New("Minimum number of shares specified is greater than the total number of shares")
 	}
 
